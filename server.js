69bea7b57d759b4e386582328d492672cddb3aa4
ThanhDN <thanhdn@fabbi.io>
2021-04-14 10:31:18 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// @ts-nocheck
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const next = require("next");
+const { nextTick } = require("process");
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({
+  dev,
+});
+const handle = nextApp.getRequestHandler();
+require("dotenv").config({
+  path: "./config.env",
+});
+const connectDb = require("./utilsServer/connectDb");
+connectDb();
+const PORT = process.env.PORT || 3000;
+
+nextApp.prepare().then(() => {
+  app.use("/api/signup", require("./api/signup"));
+  app.use("/api/auth", require("./api/auth"));
+  app.use("/api/search", require("./api/search"));
+  app.use("/api/posts", require("./api/posts"));
+  app.use("/api/profile", require("./api/profile"));
+  app.use("/api/notifications", require("./api/notifications"));
+  app.use("/api/chats", require("./api/chats"));
+  app.use("/api/reset", require("./api/reset"));
+
+  app.all("*", (req, res) => handle(req, res));
+
+  server.listen(PORT, err => {
+      if(err) throw err;
+      console.log('Server running')
+  })
+});
